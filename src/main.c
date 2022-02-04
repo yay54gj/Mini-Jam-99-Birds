81ec59132fbea51ac329cdc596d1718ef5b01443
yay54gj <yay54gj@protonmail.com>
2022-02-03 23:57:39 -0700

    goal seeking boids w/o boid behavior

--- src/main.c
+++ src/main.c
@@ -1,14 +1,58 @@
 #include "../include/raylib.h"
+#include "../include/raymath.h"
+
+#define CBOIDS 10
+#define FPS    60.0
+#define IFPS   (1.0 / 60.0)
+
+typedef struct {
+    Vector2 pos;
+    Vector2 vel;
+    Vector2 acc;
+}Boid;
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    SetRandomSeed(GetTime());
+    Boid* boids = MemAlloc(sizeof(Boid) * CBOIDS);
+    for (int i = 0; i < CBOIDS; i++){
+        boids[i].pos = (Vector2){.x = GetRandomValue(0, 600),
+                                 .y = GetRandomValue(0, 480) };
+        boids[i].vel = (Vector2){.x = (float) GetRandomValue(0, 1000) / 100.0,
+                                 .y = (float) GetRandomValue(0, 1000) / 100.0};
+        boids[i].acc = (Vector2){.x = (float) GetRandomValue(0, 1000) / 500.0,
+                                 .y = (float) GetRandomValue(0, 1000) / 500.0};
+    }
+
+    InitWindow(600, 480, "Boid");
+    HideCursor();
+    SetTargetFPS(FPS);
+
+    Vector2 mousepos;
 
     while (!WindowShouldClose())
     {
+        mousepos = GetMousePosition();
+        for (int i = 0; i < CBOIDS; i++){
+            boids[i].pos = Vector2Add(boids[i].pos, boids[i].vel);
+
+            boids[i].vel.x = Clamp(boids[i].vel.x + boids[i].acc.x, -60 * IFPS, 60 * IFPS);
+            boids[i].vel.y = Clamp(boids[i].vel.y + boids[i].acc.y, -60 * IFPS, 60 * IFPS);
+
+            //                // boid should move right?
+            boids[i].acc.x = boids[i].pos.x - mousepos.x > 0 ? 
+                                Clamp(boids[i].acc.x - 1 * IFPS, -10 * IFPS, 10 * IFPS) : 
+                                Clamp(boids[i].pos.x + 1 * IFPS, -10 * IFPS, 10 * IFPS);
+            boids[i].acc.y = boids[i].pos.y - mousepos.y > 0 ? 
+                                Clamp(boids[i].acc.y - 1 * IFPS, -10 * IFPS, 10 * IFPS) : 
+                                Clamp(boids[i].pos.y + 1 * IFPS, -10 * IFPS, 10 * IFPS);
+        }
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawCircleV(mousepos, 3, BLACK);
+            for (int i = 0; i < CBOIDS; i++){
+                DrawCircleV(boids[i].pos, 3, RED);
+            }
         EndDrawing();
     }
 
